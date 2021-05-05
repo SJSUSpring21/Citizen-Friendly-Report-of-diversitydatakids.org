b114409d36514f628267cf18e7810c443891112d
anil <anil@anil-Lenovo-G500>
2021-05-06 02:01:23 +0530

    updates.

--- server/routes/converter/convertToText.js
+++ server/routes/converter/convertToText.js
@@ -9,17 +9,18 @@ module.exports = router;
 router.post("/getRowText", (req, res) => {
   const { NLGData, info, data } = req.body;
   let text = "";
-  let min, max;
+  let min, max, avg;
   let minArray = [],maxArray=[], unknownArray = [], zeroArray = [], other = {};
   let values = Object.values(data).map((each)=>{
     return each.data
   });
   min = Math.min.apply(null,values.filter(Boolean))
   max = Math.max.apply(null,values)
+  avg = Math.round(values.reduce((a, b) => a + b) / values.length);
   //Math.min.apply(null, arr.filter(Boolean))
   for(let key in data){
     if(data.hasOwnProperty(key)){
-      if(!data[key].data){
+      if(data[key].data === null || data[key].data === undefined){
         unknownArray.push(data[key].title)
       }else if(data[key].data === 0){
         zeroArray.push(data[key].title)
@@ -40,12 +41,14 @@ router.post("/getRowText", (req, res) => {
       data: data,
       min: min,
       max: max,
+      avg: avg,
       minArray: minArray,
       maxArray: maxArray,
+      unknownArray: unknownArray,
+      zeroArray: zeroArray,
       other: other,
       cache: true
     });
-    console.log(text);
   } else {
     text = rosaenlgPug.renderFile("./routes/converter/converterRace.pug", {
       language: "en_US",
