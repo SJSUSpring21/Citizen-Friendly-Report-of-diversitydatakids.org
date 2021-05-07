be23c1103b539f4e45dc74f0b52bf4cf618ec1de
Alihussain Ladiwala <alihussain.ladiwala@sjsu.edu>
2021-05-07 03:13:20 -0700

    fixed filter

--- client/src/Pages/ResourcePage.js
+++ client/src/Pages/ResourcePage.js
@@ -28,6 +28,8 @@ import {
 import { PureComponent } from "react";
 import { PieChart, Pie, Sector } from "recharts";
 import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Autocomplete from "@material-ui/lab/Autocomplete";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -81,21 +83,34 @@ function ResourcePage(props) {
     fetchStats();
   }, [props]);
 
-  const createUrlFilters = () => {
-    const base =
-      "https://data.diversitydatakids.org/api/3/action/datastore_search";
-    myUrl = new URL(base);
-    myUrl.searchParams.append("offset", 0);
-    myUrl.searchParams.append("fields", "name");
-    myUrl.searchParams.append("distinct", "true");
-    myUrl.searchParams.append("include_total", "false");
-    myUrl.searchParams.append("resource_id", resourceId);
+  const createUrlFilters = (filter) => {
+    if (!filter) {
+      const base =
+        "https://data.diversitydatakids.org/api/3/action/datastore_search";
+      myUrl = new URL(base);
+      myUrl.searchParams.append("offset", 0);
+      myUrl.searchParams.append("fields", "name");
+      myUrl.searchParams.append("distinct", "true");
+      myUrl.searchParams.append("include_total", "false");
+      myUrl.searchParams.append("resource_id", resourceId);
+    } else {
+      const base =
+        "https://data.diversitydatakids.org/api/3/action/datastore_search";
+      myUrl = new URL(base);
+      myUrl.searchParams.append("offset", 0);
+      myUrl.searchParams.append("fields", "name");
+      myUrl.searchParams.append("distinct", "true");
+      myUrl.searchParams.append("include_total", "false");
+      myUrl.searchParams.append("resource_id", resourceId);
+      myUrl.searchParams.append("q", '{ "name": "' + filter + ':*"}');
+    }
   };
 
-  const handleChangeNameFilter = (e) => {
-    fetchRegionalStats(e.target.value);
-    fetchCkanData(e.target.value);
-    setName(e.target.value);
+  const handleChangeNameFilter = (value) => {
+    console.log(value);
+    fetchRegionalStats(value);
+    fetchCkanData(value);
+    setName(value);
     setMessage("");
   };
 
@@ -108,12 +123,12 @@ function ResourcePage(props) {
     myUrl.searchParams.append("filters", '{ "name": "' + filter + '"}');
   };
 
-  const fetchFilterData = () => {
+  const fetchFilterData = (filter) => {
     if (resourceId === "") {
       setWarning(true);
       return;
     }
-    createUrlFilters();
+    createUrlFilters(filter);
     setLoading(true);
     jsonp(myUrl.toString(), null, function (err, res) {
       let label = "";
@@ -125,10 +140,12 @@ function ResourcePage(props) {
         let name_filter = res.result.records.map((record) => {
           return record.name;
         });
-        fetchRegionalStats(name_filter[0]);
-        setName(name_filter[0]);
-        fetchCkanData(name_filter[0]);
-        setNameFilter(name_filter);
+        if (filter == undefined) {
+          fetchRegionalStats(name_filter[0]);
+          setName(name_filter[0]);
+          fetchCkanData(name_filter[0]);
+          setNameFilter(name_filter);
+        }
       }
     });
   };
@@ -159,7 +176,7 @@ function ResourcePage(props) {
     });
   };
 
-  const fetchStats = () =>{
+  const fetchStats = () => {
     const base =
       "https://data.diversitydatakids.org/api/3/action/datastore_search";
     let url = new URL(base);
@@ -194,15 +211,15 @@ function ResourcePage(props) {
             return "avg(" + each.id + ") as avg_" + each.id;
           })
           .join();
-          if(isSupported){
-            fetchStatistics();
-            fetchEthnicStats(titles,query);
-          }
+        if (isSupported) {
+          fetchStatistics();
+          fetchEthnicStats(titles, query);
         }
-      });
-  }
+      }
+    });
+  };
 
-  const fetchEthnicStats = (titles,query) => {
+  const fetchEthnicStats = (titles, query) => {
     // const base =
     //   "https://data.diversitydatakids.org/api/3/action/datastore_search";
     // let url = new URL(base);
@@ -213,57 +230,57 @@ function ResourcePage(props) {
     //   if (err) {
     //     setWarning(true);
     //   } else {
-        // let isSupported = false;
-        // let stats = res.result.fields
-        //   .filter((each) => {
-        //     if (each.id && each.id === "total_est") {
-        //       isSupported = true;
-        //     }
-        //     if (
-        //       (!each.info ||
-        //         !each.info.notes.includes(
-        //           "(only available in download file)"
-        //         )) &&
-        //       each.type === "numeric" &&
-        //       each.id !== "total_est"
-        //     ) {
-        //       return true;
-        //     }
-        //   })
-        //   .map((each) => {
-        //     titles[each.id] = each.info.label.split(";")[1]
-        //       ? each.info.label.split(";")[1]
-        //       : each.info.label.split(";")[0];
-        //     return "avg(" + each.id + ") as avg_" + each.id;
-        //   })
-        //   .join();
-        // if (isSupported) {
-          let url =
-            "https://data.diversitydatakids.org/api/3/action/datastore_search_sql?sql=SELECT " +
-            query +
-            ' from "' +
-            resourceId +
-            '"';
-          jsonp(url, null, function (err, res) {
-            if (err) {
-              setWarning(true);
-            } else {
-              if (res.success) {
-                let data = res.result.records[0];
-                Object.keys(data).length > 1 &&
-                  Axios.post("http://localhost:5000/getEthnicStats", {
-                    NLGData: NLGData,
-                    data: data,
-                    titles: titles,
-                  })
-                    .then((result) => {
-                      setEthnicStats(result.data);
-                    })
-                    .catch((error) => {});
-              }
-            }
-          });
-        // }
+    // let isSupported = false;
+    // let stats = res.result.fields
+    //   .filter((each) => {
+    //     if (each.id && each.id === "total_est") {
+    //       isSupported = true;
+    //     }
+    //     if (
+    //       (!each.info ||
+    //         !each.info.notes.includes(
+    //           "(only available in download file)"
+    //         )) &&
+    //       each.type === "numeric" &&
+    //       each.id !== "total_est"
+    //     ) {
+    //       return true;
+    //     }
+    //   })
+    //   .map((each) => {
+    //     titles[each.id] = each.info.label.split(";")[1]
+    //       ? each.info.label.split(";")[1]
+    //       : each.info.label.split(";")[0];
+    //     return "avg(" + each.id + ") as avg_" + each.id;
+    //   })
+    //   .join();
+    // if (isSupported) {
+    let url =
+      "https://data.diversitydatakids.org/api/3/action/datastore_search_sql?sql=SELECT " +
+      query +
+      ' from "' +
+      resourceId +
+      '"';
+    jsonp(url, null, function (err, res) {
+      if (err) {
+        setWarning(true);
+      } else {
+        if (res.success) {
+          let data = res.result.records[0];
+          Object.keys(data).length > 1 &&
+            Axios.post("http://localhost:5000/getEthnicStats", {
+              NLGData: NLGData,
+              data: data,
+              titles: titles,
+            })
+              .then((result) => {
+                setEthnicStats(result.data);
+              })
+              .catch((error) => {});
+        }
+      }
+    });
+    // }
     //   }
     // });
   };
@@ -507,24 +524,28 @@ function ResourcePage(props) {
       }
     });
   };
+
+  const onFilterTextChange = (value) => {
+    fetchFilterData(value);
+  };
   return (
     <div className="App">
-      <div className="name-filter-div">
-        <FormControl>
-          <InputLabel id="demo-simple-select-label">Name</InputLabel>
-          <Select
-            className="filter"
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={name}
-            onChange={handleChangeNameFilter}
-          >
-            {name_filter.map((value) => (
-              <MenuItem value={value}>{value}</MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-      </div>
+      <Autocomplete
+        id="combo-box-demo"
+        options={name_filter}
+        getOptionLabel={(option) => option}
+        style={{ width: 300 }}
+        value={name}
+        onChange={(event, newValue) => {
+          handleChangeNameFilter(newValue);
+        }}
+        onInputChange={(event, newInputValue) => {
+          onFilterTextChange(newInputValue);
+        }}
+        renderInput={(params) => (
+          <TextField {...params} label="Combo box" variant="outlined" />
+        )}
+      />
       <Snackbar open={warning} autoHideDuration={6000} onClose={closeWarning}>
         <Alert onClose={closeWarning} severity="error">
           Please enter correct resource id
